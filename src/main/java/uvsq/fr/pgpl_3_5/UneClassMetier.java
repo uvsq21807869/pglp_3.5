eba1e0fd1088e4bf8d24a04fd258108ced45a59f
uvsq21807869 <uvsq21807869@bitbucket.org>
2020-03-12 01:44:40 +0100

    implementation des methodes affiche debut et affiche fin

--- src/main/java/uvsq/fr/pgpl_3_5/UneClassMetier.java
+++ src/main/java/uvsq/fr/pgpl_3_5/UneClassMetier.java
@@ -2,4 +2,16 @@ package uvsq.fr.pgpl_3_5;
 
 public class UneClassMetier {
 
+	AfficheMetier afficheMetier;
+
+	public UneClassMetier(final AfficheMetier afficheMetier) {
+		this.afficheMetier = afficheMetier;
+	}
+	
+	
+	public void uneMethodeMetier() {
+		afficheMetier.afficheDebut(); // log message
+		//Traitements métiers
+		afficheMetier.afficheFin(); // log message
+	}
 }
